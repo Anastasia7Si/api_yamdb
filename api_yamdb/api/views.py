67cb12c5b201002c99b0dad725a015ce0c15eec2
Anastasia7Si <120373288+Anastasia7Si@users.noreply.github.com>
2023-04-25 12:15:06 +0300

    Добавлены вьюсеты

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,3 +1,46 @@
-from django.shortcuts import render
+from django.db.models import Avg
 
-# Create your views here.
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import filters, mixins, viewsets
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.permissions import (IsAuthenticatedOrReadOnly)
+
+from reviews.models import Category, Genre, Title
+from api.serializers import (CategorySerializer, GenreSerializer,
+                             TitleSerializer, TitlesViewSerializer)
+
+
+class TitlesViewSet(viewsets.ModelViewSet):
+    queryset = Title.objects.annotate(rating=Avg('reviews__score'))
+    serializer_class = TitleSerializer
+    pagination_class = PageNumberPagination
+    filter_backends = (DjangoFilterBackend,)
+
+    def get_serializer_class(self):
+        if self.action in ['list', 'retrieve']:
+            return TitlesViewSerializer
+        return TitleSerializer
+
+
+class ReviewGenreModelMixin(
+    mixins.CreateModelMixin,
+    mixins.ListModelMixin,
+    mixins.DestroyModelMixin,
+    viewsets.GenericViewSet
+):
+    permission_classes = [
+        IsAuthenticatedOrReadOnly
+    ]
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name', 'slug')
+    lookup_field = 'slug'
+
+
+class CategoriesViewSet(ReviewGenreModelMixin):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+
+class GenresViewSet(ReviewGenreModelMixin):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
