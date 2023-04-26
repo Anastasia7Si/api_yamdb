866a841abffe943eca8b922d6ee54bd3d182db99
Anastasia7Si <120373288+Anastasia7Si@users.noreply.github.com>
2023-04-26 18:49:33 +0300

    Добавлено README

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -1,6 +1,14 @@
 from rest_framework.permissions import BasePermission, SAFE_METHODS
 
 
+class AdminOnly(BasePermission):
+    def has_permission(self, request, view):
+        return (
+            request.user.is_admin
+            or request.user.is_staff
+        )
+
+
 class IsAuthorOrReadOnly(BasePermission):
     def has_permission(self, request, view):
         return (
@@ -20,5 +28,25 @@ class IsAdminOrReadOnly(BasePermission):
             or (
                 request.user.is_authenticated
                 and request.user.is_admin
+            )
+        )
+
+
+class AuthorAndStaffOrReadOnly(BasePermission):
+    def has_permission(self, request, view):
+        return (
+            request.method in SAFE_METHODS
+            or request.user.is_authenticated
+        )
+
+    def has_object_permission(self, request, view, obj):
+        return (
+            request.method in SAFE_METHODS
+            or (
+                request.user.is_authenticated
+                and (
+                    obj.author == request.user
+                    or request.user.is_moderator
+                )
             )
         )
--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,4 +1,36 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
+
+ADMIN = 'admin'
+MODERATOR = 'moderator'
+USER = 'user'
+
+ROLES = [
+    (ADMIN, ADMIN),
+    (MODERATOR, MODERATOR),
+    (USER, USER),
+]
+
+
+class User(AbstractUser):
+    email = models.EmailField(blank=True, unique=True)
+    role = models.CharField(max_length=10, choices=ROLES, default=USER)
+    bio = models.TextField(null=True, blank=True)
+    password = models.CharField(max_length=128, null=True, blank=True)
+    confirmation_code = models.CharField(max_length=200, null=True, blank=True)
+
+    @property
+    def is_user(self):
+        return self.role == USER
+
+    @property
+    def is_admin(self):
+        return self.role == ADMIN
+
+    @property
+    def is_moderator(self):
+        return self.role == MODERATOR
 
 
 class Category(models.Model):
@@ -39,3 +71,51 @@ class GenreTitle(models.Model):
 
     def __str__(self) -> str:
         return f'{self.genre} {self.title}'
+
+
+class Review(models.Model):
+    title = models.ForeignKey(
+        Title,
+        on_delete=models.CASCADE,
+        related_name='reviews'
+    )
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='reviews'
+    )
+    pub_date = models.DateTimeField(auto_now_add=True)
+    text = models.TextField()
+    score = models.IntegerField(
+        verbose_name='Оценка',
+        default=0,
+        validators=[
+            MaxValueValidator(10),
+            MinValueValidator(1)
+        ],
+    )
+
+    class Meta:
+        ordering = ['-pub_date']
+        constraints = [
+            models.UniqueConstraint(
+                fields=['author', 'title'], name="unique_review")
+        ]
+
+
+class Comment(models.Model):
+    review = models.ForeignKey(
+        Review,
+        on_delete=models.CASCADE,
+        related_name='comments'
+    )
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='comments'
+    )
+    pub_date = models.DateTimeField(auto_now_add=True)
+    text = models.TextField()
+
+    def __str__(self):
+        return self.author
